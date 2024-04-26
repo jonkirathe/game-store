a029dd8cc8aed97864e12bf816c7e4f7464060b4
John Kirathe <jnkratz@gmail.com>
2024-04-26 23:53:29 +0300

    init commi,added chakra ui, added  response layout

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+import {Grid, GridItem, Show} from "@chakra-ui/react";
+
+function App() {
+
+  return (
+    <>
+      <Grid templateAreas={{
+        base:  `"nav" "main"`,
+        lg: `"nav nav" "aside main"` //1024px
+      }}>
+        <GridItem area="nav" bg="coral">Nav</GridItem>
+          <Show above="lg">
+              <GridItem area="aside" bg="gold">Aside</GridItem>
+          </Show>
+        <GridItem area="main" bg="dodgerblue">Main</GridItem>
+      </Grid>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {ChakraProvider, extendTheme} from '@chakra-ui/react'
+import App from './App.tsx'
+import './index.css'
+
+const colors = {
+    brand: {
+        900: '#1a365d',
+        800: '#153e75',
+        700: '#2a69ac',
+    },
+}
+
+const theme = extendTheme({ colors })
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+      <ChakraProvider theme={theme}>
+          <App />
+      </ChakraProvider>
+  </React.StrictMode>,
+)
