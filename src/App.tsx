897379e9a5eaff62b8491ac4c52253d59be2b8a8
John Kirathe <jnkratz@gmail.com>
2024-04-29 10:59:47 +0300

    Sorting games

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
-import {Grid, GridItem, Show} from "@chakra-ui/react";
-import {Navbar} from "./components/Navbar.tsx";
+import {Box, Flex, Grid, GridItem, Show} from "@chakra-ui/react";
 import {GamesGrid} from "./components/GamesGrid.tsx";
-import {GenreList} from "./components/GenreList.tsx";
 import {useState} from "react";
 import {PlatformSelector} from "./components/PlatformSelector.tsx";
 import {GameQuery} from "./models/GameQuery.ts";
+import SortSelector from "./components/SortSelector.tsx";
+import {GenreList} from "./components/GenreList.tsx";
+import {Navbar} from "./components/Navbar.tsx";
 
 function App() {
     const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
@@ -21,14 +22,32 @@ function App() {
                     lg: '250px 1fr'
                 }}
             >
+               {/* <GridItem area="nav">
+                    <Navbar onSearch={(searchText) => setGameQuery({...gameQuery, searchText})}/>
+                </GridItem>*/}
                 <GridItem area="nav"><Navbar/></GridItem>
                 <Show above="lg">
                     <GridItem area="aside" paddingX={5}>
-                        <GenreList selectedGenre={gameQuery.genre} onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre})} />
+                        <GenreList selectedGenre={gameQuery.genre}
+                                   onSelectGenre={(genre) => setGameQuery({...gameQuery, genre})}/>
                     </GridItem>
                 </Show>
                 <GridItem area="main">
-                    <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) => setGameQuery({ ...gameQuery, platform}) } />
+                    <Box paddingLeft={2}>
+                        {/*<GameHeading gameQuery={gameQuery} />*/}
+
+                        <Flex marginBottom={5}>
+                            <Box marginRight={5}>
+                                <PlatformSelector selectedPlatform={gameQuery.platform}
+                                                  onSelectPlatform={(platform) => setGameQuery({
+                                                      ...gameQuery,
+                                                      platform
+                                                  })}/>
+                            </Box>
+                            <SortSelector sortOrder={gameQuery.sortOrder}
+                                          onSelectSortOrder={(sortOrder) => setGameQuery({...gameQuery, sortOrder})}/>
+                        </Flex>
+                    </Box>
                     <GamesGrid gameQuery={gameQuery}/>
                 </GridItem>
             </Grid>
--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -4,7 +4,7 @@ import apiClient, {CanceledError} from "../services/api-client.ts";
 import {FetchResponse} from "../models/FetchResponse.ts";
 import {AxiosRequestConfig} from "axios";
 
-const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig, deps?: any[]) => {
+const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig, deps?: unknown[]) => {
     const toast = useToast()
     const [data, setData] = useState<T[]>([]);
     const [isLoading, setIsLoading] = useState(false)
